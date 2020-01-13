8276567cd84ed29df889ecc9e9dfa78e1a48dfe7
ailion <1247008998@qq.com>
2020-01-13 21:19:00 +0800

    use system library targets

--- Package.swift
+++ Package.swift
@@ -14,14 +14,22 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/andy1247008998/Clibgit2.git", from: "1.0.9")
+//        .package(url: "https://github.com/andy1247008998/Clibgit2.git", from: "1.0.9")
     ],
     targets: [
+        .systemLibrary(
+            name: "Clibgit2",
+            pkgConfig: "libgit2",
+            providers: [
+                .brew(["libgit2"]),
+                .apt(["libgit2-dev"])
+            ]
+        ),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftLibGit2",
-            dependencies: []),
+            dependencies: ["Clibgit2"]),
         .testTarget(
             name: "SwiftLibGit2Tests",
             dependencies: ["SwiftLibGit2"]),
--- Sources/SwiftLibGit2/SwiftLibGit2.swift
+++ Sources/SwiftLibGit2/SwiftLibGit2.swift
@@ -1,3 +1,13 @@
-struct SwiftLibGit2 {
+import Clibgit2
+public struct SwiftLibGit2 {
     var text = "Hello, World!"
+    
+    static func clone(url: String, localPath: String, options: git_clone_options) -> OpaquePointer? {
+        var out: OpaquePointer? = nil
+        var options = options
+        git_clone(&out, url, localPath, &options)
+        return out
+    }
+    
+    
 }
